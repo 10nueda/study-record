58c8e7c2316b62a3cbca237deedac5dfb90a901f
nueda10n <nueda10n@gmail.com>
2025-08-12 08:30:18 +0900

    Todo機能更新

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,47 +1,76 @@
 import { useEffect, useState } from "react";
-import { GetAllTodos } from "./lib/todo";
+import { addTodo, deleteTodo, GetAllTodos } from "./lib/todo";
 
 export const Todo = () => {
-  const [todosData,setTodosData] = useState([]);
-  useEffect(()=>{
-    const getAllTodos = async() =>{
-      const todosData = await GetAllTodos();
-      setTodosData(todosData)
-    }
-    getAllTodos();
-  },[])
-
-  console.log(todosData);
-
   const [contents, setContents] = useState("");
-  const onChangeContents = (event) => setContents(event.target.value);
   const [time, setTime] = useState("");
-  const onChangeTime = (event) => setTime(event.target.value);
 
-  const [records, setRecords] = useState([]);
-  const newRecord = { title: contents, time: time };
+  // const [records, setRecords] = useState([]);
+  // const newRecord = { title: contents, time: time };
 
   const [error, setError] = useState("");
   const [sumTime, setSumTime] = useState(0);
 
-  const onClickAdd = () => {
+  // useState内のstate
+  const [todosData, setTodosData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  // 合計時間を算出する関数
+  const addTime = (record) => {
+    return record.reduce((acc, cur) => acc + parseInt(cur.time), 0);
+  };
+
+  useEffect(() => {
+    const getAllTodos = async () => {
+      try {
+        setIsLoading(true);
+        const todosData = await GetAllTodos();
+        setTodosData(todosData);
+        setSumTime(addTime(todosData));
+      } catch (error) {
+        console.error("読込エラーです", error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getAllTodos();
+  }, []);
+
+  // onChangeイベント
+  const onChangeContents = (event) => setContents(event.target.value);
+  const onChangeTime = (event) => setTime(event.target.value);
+
+  // データ追加イベント
+  const onClickAdd = async () => {
     if (contents === "" || time === "") {
       return setError("入力されていない項目があります");
-    } else {
-      const updatedRecords = [...records, newRecord];
-      setRecords(updatedRecords);
+    } try {
+      setError("");
+      await addTodo(contents, time);
       setContents("");
       setTime("0");
-      setError("");
 
-      const addTime = (record) => {
-        return record.reduce((acc, cur) => acc + parseInt(cur.time), 0);
-      };
-      setSumTime(addTime(updatedRecords));
+      const updatedTodos = await GetAllTodos();
+      setTodosData(updatedTodos);
+      setSumTime(addTime(updatedTodos));
+    } catch (err) {
+      setError("登録に失敗しました");
+      console.error(err);
     }
   };
 
-  return (
+  // データ削除イベント
+  const onClickDelete = async (id) => {
+    await deleteTodo(id);
+    const updatedTodos = await GetAllTodos();
+    setTodosData(updatedTodos);
+    setSumTime(addTime(updatedTodos));
+  }
+
+  // 画面描写
+  return isLoading ? (
+    <div>Loading...</div>
+  ) : (
     <>
       <div>
         <h1>学習記録一覧</h1>
@@ -60,30 +89,20 @@ export const Todo = () => {
           <span>入力されている時間：{time}</span>
         </div>
         <div>
-          {todosData.map((todo,id) =>{
-            return (
-              <div key={id}>
-                <p>
-                  {todo.contents}
-                  {todo.time}時間
-                </p>
-              </div>
-            );
-          })}
-        </div>
-        <div>
-          {records.map((record, index) => {
+          {todosData.map((todo, index) => {
             return (
               <div key={index}>
                 <p>
-                  {record.title}
-                  {record.time}時間
+                  {todo.contents}
+                  {todo.time}時間
+                  <button onClick={() => onClickDelete(todo.id)}>削除</button>
                 </p>
               </div>
             );
           })}
         </div>
         <button onClick={onClickAdd}>登録</button>
+        <span> </span>
         <p>合計時間：{sumTime}/1000h</p>
         <div>{error}</div>
       </div>
--- src/lib/todo.js
+++ src/lib/todo.js
@@ -1,9 +1,17 @@
 import { supabase } from "../utils/supabase";
 
-export async function GetAllTodos(){
+export async function GetAllTodos() {
     const response = await supabase.from("study-record").select("*");
-    if(response.error){
+    if (response.error) {
         throw new Error(response.error.message);
     }
     return response.data;
+}
+
+export const addTodo = async (title, time) => {
+    await supabase.from("study-record").insert({ contents: title, time: time })
+}
+
+export const deleteTodo = async (id) => {
+    await supabase.from("study-record").delete().eq("id", id)
 }
